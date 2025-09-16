87529ca1db98a852564e038914b9e6861ac4cbe5
luke <luke.e.hearon@gmail.com>
2025-09-16 15:48:10 -0400

    fix typos, improve citation examples in manuscript.qmd

--- code/shaping/01_join_data.R
+++ code/shaping/01_join_data.R
@@ -1,11 +1,12 @@
 library(dplyr)
 
+data_type <- 'partial'
 dir_raw <- './data/raw'
 
-tasting <- file.path(dir_raw, 'tasting_full.csv') %>% 
+tasting <- file.path(dir_raw, paste0('tasting_', data_type, '.csv')) %>% 
   read.csv()
 
-survey <- file.path(dir_raw, 'survey_full.csv') %>% 
+survey <- file.path(dir_raw, paste0('survey_', data_type, '.csv')) %>% 
   read.csv()
 
 data_join <- full_join(tasting, survey, by='participant_id')
